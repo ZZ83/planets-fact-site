f8edba27de57dff85dc2d3437bba48952c91c215
Zekel <13670661+ZZ83@users.noreply.github.com>
2021-12-30 10:19:06 -0800

    Add stats and description to their own files

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function openSlideMenu() {
-    const togglePrimary = document.querySelector('.primary-navigation');
     const toggle        = document.querySelector('.primary-navigation__inner');
     const toggleMobile  = document.querySelector('.mobile-nav-button__menu');
-
     if (toggle.classList.contains("primary-navigation__inner--open")) {
         toggle.classList.remove("primary-navigation__inner--open");
     } else {
         toggle.classList.add("primary-navigation__inner--open");
     }
-
     if (toggleMobile.classList.contains("mobile-nav-button__menu--open")) {
         toggleMobile.classList.remove("mobile-nav-button__menu--open");
     } else {
