4e5a7d9b110871d0696978755cbc947ca4494944
Zekel <13670661+ZZ83@users.noreply.github.com>
2022-01-10 12:33:17 -0800

    Rename aria-controls in html files

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function openSlideMenu() {
-    const toggle        = document.querySelector('.primary-navigation__inner');
-    const toggleMobile  = document.querySelector('.mobile-nav-button__menu');
+    const toggle           = document.querySelector('.primary-navigation__inner');
+    const toggleMobile     = document.querySelector('.mobile-nav-button__menu');
+    const mobileNavButton  = document.querySelector('.mobile-nav-button');
     if (toggle.classList.contains("primary-navigation__inner--open")) {
         toggle.classList.remove("primary-navigation__inner--open");
     } else {
         toggle.classList.add("primary-navigation__inner--open");
+    }
+    if(mobileNavButton.getAttribute("aria-expanded") === "false") {
+        mobileNavButton.setAttribute("aria-expanded", true);
+    } else {
+        mobileNavButton.setAttribute("aria-expanded", false);
     }
     if (toggleMobile.classList.contains("mobile-nav-button__menu--open")) {
         toggleMobile.classList.remove("mobile-nav-button__menu--open");
@@ -12,6 +18,5 @@ function openSlideMenu() {
         toggleMobile.classList.add("mobile-nav-button__menu--open");
     }
 }
-
 document.querySelector('.mobile-nav-button').addEventListener("click", openSlideMenu);
     
